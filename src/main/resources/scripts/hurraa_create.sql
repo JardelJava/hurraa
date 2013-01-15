de6c28b685d328fb8e2168b7e2ea61a0d445dcaa
heliofrota <heliofrota@20b042bf-ae08-cc53-fde0-2f7ce35d133d>
2013-01-15 21:10:25 +0000

    Script de inicial para testar estrutura básica.
    git-svn-id: http://hurraa.googlecode.com/svn/trunk/hurraa@54 20b042bf-ae08-cc53-fde0-2f7ce35d133d

--- src/main/resources/scripts/hurraa_create.sql
+++ src/main/resources/scripts/hurraa_create.sql
@@ -3,9 +3,10 @@ CREATE DATABASE "hurraa"
   ENCODING 'UTF8'
   LC_COLLATE = 'en_US.UTF-8'
   LC_CTYPE = 'en_US.UTF-8';
-  
+
 CREATE TABLE fabricante (
   id serial PRIMARY KEY,
   nome text NOT NULL,
   tipo smallint
-);  
+);
+
