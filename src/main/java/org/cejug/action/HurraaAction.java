63aac30d209706a18f5bb34c88d6dbb047d3d790
heliofrota <heliofrota@20b042bf-ae08-cc53-fde0-2f7ce35d133d>
2013-01-25 19:28:24 +0000

    Javadoc.
    git-svn-id: http://hurraa.googlecode.com/svn/trunk/hurraa@132 20b042bf-ae08-cc53-fde0-2f7ce35d133d

--- src/main/java/org/cejug/action/HurraaAction.java
+++ src/main/java/org/cejug/action/HurraaAction.java
@@ -2,8 +2,17 @@ package org.cejug.action;
 
 import org.mentawai.core.BaseAction;
 
+/**
+ * Action HurraaAction.
+ *
+ * @author helio frota
+ *
+ */
 public class HurraaAction extends BaseAction {
 
+	/**
+	 * Metodo default execute.
+	 */ 
 	public String execute() {
 		return SUCCESS;
 	}
