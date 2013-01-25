9655e978e1c82aecebfe25f60793d9fd536b3390
heliofrota <heliofrota@20b042bf-ae08-cc53-fde0-2f7ce35d133d>
2013-01-25 16:28:11 +0000

    Javadoc.
    git-svn-id: http://hurraa.googlecode.com/svn/trunk/hurraa@121 20b042bf-ae08-cc53-fde0-2f7ce35d133d

--- src/main/java/org/cejug/action/administracao/FabricantesAction.java
+++ src/main/java/org/cejug/action/administracao/FabricantesAction.java
@@ -4,23 +4,36 @@ import org.cejug.business.InventarioBusiness;
 import org.mentawai.core.BaseAction;
 
 /**
- * Action FabricanteAction.
+ * Action FabricantesAction.
  *
  * @author helio frota
  *
  */
 public class FabricantesAction extends BaseAction {
 
+	/**
+	 * Classe de negocio injetada pelo mentaContainer.
+	 */
 	private InventarioBusiness inventarioBusiness;
 
+	/**
+	 * Construtor parametrico usando constructor injection.
+	 * @param inventarioBusiness InventarioBusiness
+	 */
 	public FabricantesAction(InventarioBusiness inventarioBusiness) {
 		this.inventarioBusiness = inventarioBusiness;
 	}
 
+	/**
+	 * Metodo default execute.
+	 */
 	public String execute() {
 		return SUCCESS;
 	}
 
+	/**
+	 * Retorna lista de fabricantes no formato JSON com determinado limite e offset.
+	 */
 	public void getFabricantes() {
 		ajax(inventarioBusiness.getFabricantes(input.getInt("inicio"), input.getInt("limite")));
 	}
