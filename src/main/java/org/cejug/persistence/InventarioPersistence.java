02bfcdf4d09ddde505c1917d77dd21c606a945f4
heliofrota <heliofrota@20b042bf-ae08-cc53-fde0-2f7ce35d133d>
2013-01-25 16:24:28 +0000

    Javadoc.
    git-svn-id: http://hurraa.googlecode.com/svn/trunk/hurraa@119 20b042bf-ae08-cc53-fde0-2f7ce35d133d

--- src/main/java/org/cejug/persistence/InventarioPersistence.java
+++ src/main/java/org/cejug/persistence/InventarioPersistence.java
@@ -4,8 +4,20 @@ import java.util.List;
 
 import org.cejug.pojo.Fabricante;
 
+/**
+ * Interface de persistencia InventarioPersistence.
+ *
+ * @author helio frota
+ *
+ */
 public interface InventarioPersistence {
 
+	/**
+	 * Busca fabricantes na base de dados com determinado limite e offset.
+	 * @param inicio int
+	 * @param limite int
+	 * @return List < Fabricante >
+	 */
 	public List < Fabricante > getFabricantes(int inicio, int limite);
 
 }
--- src/main/java/org/cejug/persistence/impl/InventarioPersistenceImpl.java
+++ src/main/java/org/cejug/persistence/impl/InventarioPersistenceImpl.java
@@ -7,14 +7,33 @@ import javax.persistence.EntityManager;
 import org.cejug.persistence.InventarioPersistence;
 import org.cejug.pojo.Fabricante;
 
+/**
+ * Classe de persistencia InventarioPersistenceImpl.
+ * @author helio frota
+ *
+ */
 public class InventarioPersistenceImpl implements InventarioPersistence {
 
+	/**
+	 * EntityManager injetado pelo mentaContainer.
+	 */ 
 	private EntityManager entityManager;
 
+	/**
+	 * Construtor parametrico.
+	 *
+	 * @param entityManager EntityManager
+	 */ 
 	public InventarioPersistenceImpl(EntityManager entityManager) {
 		this.entityManager = entityManager;
 	}
-
+	
+	/**
+	 * Busca fabricantes na base de dados com determinado limite e offset.
+	 * @param inicio int
+	 * @param limite int
+	 * @return List < Fabricante >
+	 */
 	public List < Fabricante > getFabricantes(int inicio, int limite) {
 		String query = "select f from Fabricante f";
 		return entityManager.createQuery(query, Fabricante.class)
