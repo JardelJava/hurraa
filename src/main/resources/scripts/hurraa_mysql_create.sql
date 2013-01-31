5230e23c065ae10a5c9259316c0548dfad7b01af
me@hildeberto.com <me@hildeberto.com@20b042bf-ae08-cc53-fde0-2f7ce35d133d>
2013-01-31 09:45:14 +0000

    Script para a criacao do banco de dados no MySQL.
    git-svn-id: http://hurraa.googlecode.com/svn/trunk/hurraa@176 20b042bf-ae08-cc53-fde0-2f7ce35d133d

---
+++ src/main/resources/scripts/hurraa_mysql_create.sql
@@ -0,0 +1,49 @@
+drop table if exists fabricante;
+
+create table fabricante (
+  id   char(32)    not null,
+  nome varchar(50) not null,
+  tipo tinyint(1)      null
+) engine innodb;
+
+alter table fabricante add constraint pk_fabricante primary key (id);
+
+create table usuario (
+  id	char(32)     not null,   
+  nome	varchar(100) not null,
+  email	varchar(100) not null,
+  fone	varchar(10)      null
+) engine innodb;
+
+alter table usuario add constraint pk_usuario primary key (id);
+
+create table autenticacao (
+  id         char(32)     not null,
+  usuario_id char(32)     not null,
+  email      varchar(100) not null,
+  senha      varchar(50)  not null,
+) engine innodb;
+
+alter table autenticacao add constraint pk_autenticacao primary key (id);
+create unique index idx_unique_autenticacao_email ON autenticacao (email);
+alter table autenticacao add constraint fk_usuario_autenticacao foreign key (usuario_id) references usuario on delete cascade;
+
+create table grupo (
+    id   char(32)    not null,   
+    nome varchar(50) not null
+) engine innodb;
+
+alter table grupo add constraint pk_grupo primary key (id);
+create unique index idx_unique_grupo_nome on grupo (nome);
+
+create table usuario_grupo (
+    id         char(32) not null,
+    grupo_id   char(32) not null,
+    usuario_id char(32) not null
+) engine innodb;
+
+alter table usuario_grupo add constraint pk_usuario_grupo primary key (id);
+create index idx_grupo_usuario on usuario_grupo (grupo_id);
+create index idx_usuario_grupo on usuario_grupo (usuario_id);
+alter table usuario_grupo add constraint fk_grupo_usuario foreign key (grupo_id) references grupo on delete cascade;
+alter table usuario_grupo add constraint fk_usuario_grupo foreign key (usuario_id) references usuario on delete cascade;
