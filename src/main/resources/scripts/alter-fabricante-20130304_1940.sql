b792e1a0e9fd2669714e693c66b0a41bfa46cae1
heliofrota <heliofrota@20b042bf-ae08-cc53-fde0-2f7ce35d133d>
2013-03-09 04:09:27 +0000

    changed to modify column.
    git-svn-id: http://hurraa.googlecode.com/svn/trunk/hurraa@207 20b042bf-ae08-cc53-fde0-2f7ce35d133d

--- src/main/resources/scripts/alter-fabricante-20130304_1940.sql
+++ src/main/resources/scripts/alter-fabricante-20130304_1940.sql
@@ -1,4 +1,4 @@
-alter table fabricante alter column tipo type varchar(30);
+alter table fabricante modify column tipo varchar(30);
 
 update fabricante set tipo = 'HARDWARE' where tipo = '1';
 update fabricante set tipo = 'SOFTWARE' where tipo = '2';
