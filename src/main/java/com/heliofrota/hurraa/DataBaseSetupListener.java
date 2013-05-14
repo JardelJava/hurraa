3898e443676f8cd48c66afeae017fe7a17cd561a
Roberto Coelho <roberto.coelho@gmail.com>
2013-05-14 00:39:07 -0300

    Criando os inserts de autenticação.

--- src/main/java/com/heliofrota/hurraa/DataBaseSetupListener.java
+++ src/main/java/com/heliofrota/hurraa/DataBaseSetupListener.java
@@ -5,6 +5,7 @@ import static org.mentalog.Log.Info;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.servlet.ServletContextEvent;
@@ -12,6 +13,8 @@ import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
 import org.eclipse.persistence.config.PersistenceUnitProperties;
+
+import com.heliofrota.hurraa.entity.administracao.Autenticacao;
 
 /**
  * Listener que cria o contexto do JPA para geração do DDL.
@@ -32,11 +35,20 @@ public class DataBaseSetupListener implements ServletContextListener {
 		Info.log("database setup...");
 
 		Map<String, String> persistenceUnitProperties = new HashMap<>();
+		
 		persistenceUnitProperties.put(PersistenceUnitProperties.DDL_GENERATION, "create-tables");
 		persistenceUnitProperties.put(PersistenceUnitProperties.DDL_GENERATION_MODE, "database");
 
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hurraa", persistenceUnitProperties);
 		emf.createEntityManager();
+		EntityManager em = emf.createEntityManager(persistenceUnitProperties);
+		Autenticacao auth = new Autenticacao();
+		auth.setId("1");
+		auth.setEmail("admin@hurra.org");
+		auth.setSenha("admin");
+		em.getTransaction().begin();
+		em.persist(auth);
+		em.getTransaction().commit();		
 		emf.close();
 
 		Info.log("database created.");
